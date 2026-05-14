c1527d1b01f3d9fdcd9204021b002b9647d48c3c
magenta <maajenta@proton.me>
2026-05-13 21:47:59 -0400

    Permissions of keys changes at entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,6 @@
 if [ ! -f /etc/ssh/sshd_config ]; then
     cp -r /etc/ssh.bak/. /etc/ssh/
 fi
-exec /usr/sbin/sshd -D
+chown git:git /home/git/.ssh/authorized_keys
+chmod 600 /home/git/.ssh/authorized_keys
+exec /usr/sbin/sshd -D -e
